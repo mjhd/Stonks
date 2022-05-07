36591caf8be2d580e6077f8de0e3ef7c552b5572
radmadical Hdev <>
2022-05-07 23:46:40 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+public class Stocks
+{
+	public static int SValue;
+    public static void Main() 
+	  {
+      Console.WriteLine("STOCK EXCHANGE!");
+      /*List<Stock> stocks = new List<Stock>();
+	    Console.WriteLine("STOCK EXCHANGE!");
+	    for(int i = 35; i > 0; i++)
+	    {
+	        Random rnd = new Random();
+	        string SSS = "";
+	        string SS = "";
+	        int delay = rnd.Next(2,6);
+	        while(delay != 0)
+	        {
+	            SS = Char.ConvertFromUtf32(rnd.Next(65,90));
+	            SSS = (SSS + SS);
+			delay = (delay - 1);
+	        }
+	        SValue = rnd.Next(1,10000);
+	        Console.ForegroundColor = ConsoleColor.Green;
+	        //implement spacing wiht while loop and [] brackets to see amount of charchters in the string.
+            stocks.Add(new Stock() { StockSymbol = SSS, StockValue = SValue });
+            //use same format as below but also make a random stock getter and change valu on the list of stock value then console.writeline the format below.
+	        //string StockFinal = ("Symbol: " + SSS + " | Value: " + SValue + Spacing +" | ↑ |");
+	        
+	        Console.WriteLine("-----------------------------------");
+	        i = i - 2;
+	    }
+      */
+	}
+}
