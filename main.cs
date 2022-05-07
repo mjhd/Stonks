f9e31aa153f85d433a8665e315ee4595ef484ad3
radmadical Hdev <thedesignlabproject@gmail.com>
2022-05-07 23:49:03 +0000

    I pooped - eets broke

--- main.cs
+++ main.cs
@@ -1,12 +1,9 @@
 using System;
-using System.Collections.Generic;
 public class Stocks
 {
-	public static int SValue;
     public static void Main() 
 	  {
-      Console.WriteLine("STOCK EXCHANGE!");
-      /*List<Stock> stocks = new List<Stock>();
+      int SValue;
 	    Console.WriteLine("STOCK EXCHANGE!");
 	    for(int i = 35; i > 0; i++)
 	    {
@@ -22,14 +19,10 @@ public class Stocks
 	        }
 	        SValue = rnd.Next(1,10000);
 	        Console.ForegroundColor = ConsoleColor.Green;
-	        //implement spacing wiht while loop and [] brackets to see amount of charchters in the string.
-            stocks.Add(new Stock() { StockSymbol = SSS, StockValue = SValue });
-            //use same format as below but also make a random stock getter and change valu on the list of stock value then console.writeline the format below.
-	        //string StockFinal = ("Symbol: " + SSS + " | Value: " + SValue + Spacing +" | ↑ |");
+	        Console.WriteLine("Symbol: " + SSS + " | Value: " + SValue + " | ↑ |");
 	        
 	        Console.WriteLine("-----------------------------------");
 	        i = i - 2;
 	    }
-      */
 	}
 }
