c9b20d17a1dd915822c017e3c5c738cd1a27830a
radmadical Hdev <thedesignlabproject@gmail.com>
2022-05-08 00:34:15 +0000

    Buncha testy poopoo

--- main.cs
+++ main.cs
@@ -1,8 +1,29 @@
 using System;
+using System.Timers;
 public class Stocks
 {
+  private static System.Timers.Timer aTimer;
+
+  private static void SetTimer()
+   {
+        // Create a timer with a two second interval.
+        aTimer = new System.Timers.Timer(20000);
+        // Hook up the Elapsed event for the timer. 
+        aTimer.Elapsed += OnTimedEvent;
+        aTimer.AutoReset = true;
+        aTimer.Enabled = true;
+    }
+
+    private static void OnTimedEvent(Object source, ElapsedEventArgs e)
+    {
+        Console.WriteLine("The Elapsed event was raised at {0:HH:mm:ss.fff}",
+                          e.SignalTime);
+    }
+
     public static void Main()
 	  {
+      SetTimer();
+      
       int SValue;
 	    Console.WriteLine("STOCK EXCHANGE!");
 	    for(int i = 35; i > 0; i++)
@@ -19,8 +40,9 @@ public class Stocks
 	        }
 	        SValue = rnd.Next(1,10000);
 	        Console.ForegroundColor = ConsoleColor.Green;
-	        Console.WriteLine("Symbol: " + SSS + " | Value: " + SValue + " | ↑ |");
-	        Console.WriteLine("-----------------------------------");
+	        Console.WriteLine("Symbol: " + SSS + " | Value: " + SValue + "$ | ↑ |");
+	        Console.ForegroundColor = ConsoleColor.White;
+	        Console.WriteLine("------------------------------------");
 	        i = i - 2;
 	    }
 	}
